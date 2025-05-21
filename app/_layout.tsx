6aaa862b9ecafad66462fdb41adca2ecbc0784e1
Moein Shafiei <mshafiei@eckler.ca>
2025-05-21 19:45:30 -0400

    Add drawer and put settings in it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Stack } from "expo-router";
+import { Drawer } from 'expo-router/drawer';
 
-export default function RootLayout()
-{
-    return <Stack />;
+export default function RootLayout() {
+    return (
+        <Drawer>
+            <Drawer.Screen name="index" options={{ title: 'Home' }} />
+            <Drawer.Screen name="settings" options={{ title: 'Settings' }} />
+        </Drawer>
+    );
 }
